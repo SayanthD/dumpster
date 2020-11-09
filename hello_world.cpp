19c4769bd83742351c36ae27e616abd5f89d9787
SayanthD <sayanthd@gmail.com>
2020-11-09 11:38:29 +0530

    Level 0

---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+// Print "Hello, World!" and exit
+
+#include <iostream>
+// Standard Input / Output Streams Library
+
+int main() {
+	std::cout << "Hello, World!"; // standard output stream
+	return 0; // exit status
+}
