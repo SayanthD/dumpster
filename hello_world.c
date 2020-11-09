8f146bae44fc434cd8e6517e9f7a1516f9543a62
SayanthD <sayanthd@gmail.com>
2020-11-09 11:47:58 +0530

    Level 0

---
+++ hello_world.c
@@ -0,0 +1,10 @@
+// Print "Hello, World!" and exit
+
+#include <stdio.h>
+// Standard Input / Output Streams Library
+
+int main(void)
+{
+  printf("Hello, World!"); // print statement
+  return 0; // exit status
+}
