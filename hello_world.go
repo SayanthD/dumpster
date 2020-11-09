f8e814b5d3bbfecd1a5399f9110a479c487b2c10
SayanthD <sayanthd@gmail.com>
2020-11-09 12:08:10 +0530

    Level 0

---
+++ hello_world.go
@@ -0,0 +1,9 @@
+// Print "Hello, World!" and exit
+
+package main // Package Declaration
+
+import "fmt" // Include "fmt" package
+
+func main() {
+    fmt.Println("Hello, World!") // print statement
+}
